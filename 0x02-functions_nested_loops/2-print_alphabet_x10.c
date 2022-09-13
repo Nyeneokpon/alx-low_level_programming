f27de8f80429a2c17854f59a07af8c84964926f7
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-13 14:00:13 -0700

    Craft

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet in lowercase
  *
- * Return: Always 0
+ * Return: Always void
  */
 void print_alphabet_x10(void)
 {
@@ -16,5 +16,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
