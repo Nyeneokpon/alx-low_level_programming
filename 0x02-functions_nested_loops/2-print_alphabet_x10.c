0750e168d10de8dfaad9267c7f55c96603cc284d
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-13 13:54:07 -0700

    Ben

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int n, a;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		for (a = 97; a <= 122; a++)
+		{	
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _putchar(char k);
 
 void print_alphabet(void);
+
+oid print_alphabet_x10(void);
