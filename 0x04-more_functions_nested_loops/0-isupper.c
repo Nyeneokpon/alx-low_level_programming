e7342e3121b84590595678f76cee3c425d680277
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-16 04:20:27 -0700

    Mean

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: an integer parameter
+ * Return: something but success
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning about Nested Loop in C
