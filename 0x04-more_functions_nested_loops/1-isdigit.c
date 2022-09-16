dd7f07d6b3f18166fbb17b3a53a0e3321f7e181a
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-16 04:37:05 -0700

    means

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: is an integer  argument
+ * Return: success
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
