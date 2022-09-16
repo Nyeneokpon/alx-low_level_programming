32333f2036b2656a690273b6f415a22c736c4c8b
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-16 08:50:17 -0700

    fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,12 @@ void print_diagonal(int n)
 	else
 	{
 		for (x = 0; x < n; x++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n);
+		{
+			for (i = 0; i < x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n'):
 }
