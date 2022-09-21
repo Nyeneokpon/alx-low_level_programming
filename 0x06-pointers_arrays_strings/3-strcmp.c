bd0daf0e432313f972e4c6ebc69803aeaec5ddf9
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-21 08:10:04 -0700

    Run

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ *
+ *
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reverses the content of an array of integers
+ * @a: array a
+ * @n: element of an array
+ */
+
+void reverse_array(int *a, int n)
+{
+
+}
