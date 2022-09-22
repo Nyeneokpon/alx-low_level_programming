a020018e3577ba3b00e686595f9f19f666e45359
Nyeneokpon <ekanem.nyeneokpon@gmail.com>
2022-09-22 14:26:18 -0700

    Thanks Betty

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: integer parameter
+ */
+
+void print_number(int n)
+{
+	unsigned int m, i, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+	i = m;
+	count = 1;
+
+	while (i > 9)
+	{
+		i /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
